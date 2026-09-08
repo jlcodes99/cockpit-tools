0bd25155562864018d6f552969bc5ad63b6b9ff5
jlcodes99 <224477852+jlcodes99@users.noreply.github.com>
2026-09-08 12:22:13 +0000

    chore(homebrew): update cask for v1.3.45

--- Casks/cockpit-tools.rb
+++ Casks/cockpit-tools.rb
@@ -1,6 +1,6 @@
 cask "cockpit-tools" do
-  version "1.3.42"
-  sha256 "7d94e6a4282247fee078532bd9d197262a65baa2daf6458240491fa295f0c119"
+  version "1.3.45"
+  sha256 "23bbd97df9393643d86a013104f59e8b084541bad86a7899b3891d3ece219f04"
 
   url "https://github.com/jlcodes99/cockpit-tools/releases/download/v#{version}/Cockpit.Tools_#{version}_universal.dmg"
   name "Cockpit Tools"
